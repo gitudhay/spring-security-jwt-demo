fa9cf4fcc4b2a15a2a98c8e792cde85a401a92bb
udhayakumar.g+1-at-181833654912 <udhayakumar.g@botree.co.in>
2025-07-28 14:43:32 +0530

    User data from H2 database with default schema.

--- src/main/java/com/uk/spring_security_phase1/SecurityConfiguration.java
+++ src/main/java/com/uk/spring_security_phase1/SecurityConfiguration.java
@@ -2,14 +2,22 @@ package com.uk.spring_security_phase1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 /**
  * Security configuration class.
@@ -17,6 +25,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,17 +47,19 @@ public class SecurityConfiguration {
      * @return an instance of {@link InMemoryUserDetailsManager} containing user credentials and roles.
      */
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("user")
-                        .password(passwordEncoder().encode("footest"))
-                        .roles("USER")
-                        .build(),
-                User.withUsername("admin")
-                        .password(passwordEncoder().encode("fooadmin"))
-                        .roles("ADMIN")
-                        .build()
-        );
+    public UserDetailsManager userDetailsService() {
+        UserDetails userDetails1 = User.withUsername("user")
+                .password(passwordEncoder().encode("footest"))
+                .roles("USER")
+                .build();
+        UserDetails userDetails2 = User.withUsername("admin")
+                .password(passwordEncoder().encode("fooadmin"))
+                .roles("ADMIN")
+                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource());
+        jdbcUserDetailsManager.createUser(userDetails1);
+        jdbcUserDetailsManager.createUser(userDetails2);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
